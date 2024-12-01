7c26e5e33fd2bb291e182b398d9a85c1e3ce770d
MongXin <1393907820@qq.com>
2024-12-01 17:25:51 +0800

    Update JueJinP15-最少前缀操作问题.cpp
    又一个学术垃圾hhhh

--- 双指针/JueJinP15-最少前缀操作问题.cpp
+++ 双指针/JueJinP15-最少前缀操作问题.cpp
@@ -2,13 +2,31 @@
 #include <string>
 using namespace std;
 
+bool noCommon(string S,string T){
+    for (int i = 0; i < S.size(); i++) {
+    // 遍历 T 中的每个字符
+        for (int j = 0; j < T.size(); j++) {
+            // 如果发现相同的字符，返回 false
+            if (S[i] == T[j]) {
+                return false;
+            }
+        }
+    }
+    // 如果没有发现相同的字符，返回 true
+    return true;
+}
+
 int solution(string S, string T) {
     int i = 0, j = 0;  // 初始化两个指针
     int operations = 0;  // 初始化操作次数
+    if(S.size()>T.size()&&!noCommon(S,T)){
+        return (S.size()>T.size())?S.size():T.size();
+    }
     if(S.size()>T.size()){
         while(i<S.size()+1&&j<T.size()+1){
             if(S[i++]==T[j++]){
-            }else {
+            }
+            else {
             operations++;
             }
         }
@@ -28,6 +46,7 @@ int solution(string S, string T) {
 }
 
 int main() {
+    cout<<(solution("jnjsmjgcyplywiwmb","eefgegffege")==17)<<endl;
     cout << (solution("aba", "abb") == 1) << endl;
     cout << (solution("abcd", "efg") == 4) << endl;
     cout << (solution("xyz", "xy") == 1) << endl;
